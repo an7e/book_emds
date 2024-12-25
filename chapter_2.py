ba29268fecc9705943f21136471b15296660bb1e
an7e <an7e@outlook.com>
2024-12-26 01:06:47 +0300

    Add chapter 2

--- chapter_2.py
+++ chapter_2.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from book-emds!")
+from scipy.stats import binom
+from scipy.stats import beta
 
+# Binomial Distribution
+n = 10
+p = 0.9
+q = 0
+for k in range(n + 1):
+    probability = binom.pmf(k, n, p)
+    print("{0} - {1}".format(k, probability))
 
-if __name__ == "__main__":
-    main()
+# Beta Distribution
+a = 8
+b = 2
+p = beta.cdf(.90, a, b)
+print(p)
+a = 30
+b = 6
+p = 1.0 - beta.cdf(.90, a, b)
+print(p)
+a = 8
+b = 2
+p = beta.cdf(.90, a, b) - beta.cdf(.80, a, b)
+print(p)
